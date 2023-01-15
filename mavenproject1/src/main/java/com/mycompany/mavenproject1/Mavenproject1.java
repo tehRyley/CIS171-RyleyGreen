8454bcd8bc92ebdb9d3b7207e1144679b626a23e
tehRyley <rg0901375@otc.edu>
2023-01-15 15:37:22 -0600

    Testing commit via NetBeans

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Ryley
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("A new line");
+    }
+}
