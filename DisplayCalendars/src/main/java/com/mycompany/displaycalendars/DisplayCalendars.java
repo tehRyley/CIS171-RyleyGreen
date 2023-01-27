8cd00bbf66f820f0d89246ec68fecb69bf28762b
tehRyley <rg0901375@otc.edu>
2023-01-27 17:59:38 -0600

    Rebased on GregorianCalendar import Removed "getMonthName" method Reduced "getNumberOfDaysInMonth" method Rephrased additional output

--- DisplayCalendars/src/main/java/com/mycompany/displaycalendars/DisplayCalendars.java
+++ DisplayCalendars/src/main/java/com/mycompany/displaycalendars/DisplayCalendars.java
@@ -1,6 +1,9 @@
 package com.mycompany.displaycalendars;
 
 import java.util.Scanner;
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  *
@@ -25,7 +28,8 @@ public class DisplayCalendars {
         // Print calendar for the month of the year
         printMonth(year, month);
         // Print Amount of days found in the entered month of the year
-        System.out.println("\n" + getMonthName(month) + ", " + year + " contains " + getNumberOfDaysInMonth(year, month) + " days");
+        Calendar userCal = new GregorianCalendar(year, month, 0); 
+        System.out.println("\n" + userCal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()) + ", " + userCal.get(Calendar.YEAR) + " contains " + userCal.getActualMaximum(Calendar.DAY_OF_MONTH) + " days");
     }
 
     /**
@@ -43,56 +47,10 @@ public class DisplayCalendars {
      * Print the month title, e.g., May, 1999
      */
     public static void printMonthTitle(int year, int month) {
-        System.out.println("         " + getMonthName(month)
-                + ", " + year);
+        Calendar userCal = new GregorianCalendar(year, month, 0);
+        System.out.println("         " + userCal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()) + ", " + year);
         System.out.println("-----------------------------");
         System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
-    }
-
-    /**
-     * Get the English name for the month
-     */
-    public static String getMonthName(int month) {
-        String monthName = "";
-        switch (month) {
-            case 1:
-                monthName = "January";
-                break;
-            case 2:
-                monthName = "February";
-                break;
-            case 3:
-                monthName = "March";
-                break;
-            case 4:
-                monthName = "April";
-                break;
-            case 5:
-                monthName = "May";
-                break;
-            case 6:
-                monthName = "June";
-                break;
-            case 7:
-                monthName = "July";
-                break;
-            case 8:
-                monthName = "August";
-                break;
-            case 9:
-                monthName = "September";
-                break;
-            case 10:
-                monthName = "October";
-                break;
-            case 11:
-                monthName = "November";
-                break;
-            case 12:
-                monthName = "December";
-        }
-
-        return monthName;
     }
 
     /**
@@ -161,20 +119,8 @@ public class DisplayCalendars {
      * Get the number of days in a month
      */
     public static int getNumberOfDaysInMonth(int year, int month) {
-        if (month == 1 || month == 3 || month == 5 || month == 7
-                || month == 8 || month == 10 || month == 12) {
-            return 31;
-        }
-
-        if (month == 4 || month == 6 || month == 9 || month == 11) {
-            return 30;
-        }
-
-        if (month == 2) {
-            return isLeapYear(year) ? 29 : 28;
-        }
-
-        return 0; // If month is incorrect
+        Calendar userCal = new GregorianCalendar(year, month, 0);
+        return userCal.getActualMaximum(Calendar.DAY_OF_MONTH);
     }
 
     /**
