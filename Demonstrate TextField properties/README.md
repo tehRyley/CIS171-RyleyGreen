2f828914e3ea4929c983e645fe3e2811bb1f62c4
tehRyley <rg0901375@otc.edu>
2023-02-28 21:12:58 -0600

    Add Text Field project

---
+++ Demonstrate TextField properties/README.md
@@ -0,0 +1,54 @@
+# Exercise 16.6 Demonstrate Text field properties
+
+Write a program that sets the horizontal-alignment and column-size properties of a text field dynamically.
+
+## Example Output
+
+![Sample Output](README.jpg)
+
+## Analysis Steps
+
+Follow the steps needed to create a text field with options to change column size and alignment dynamically.
+
+### Design
+
+a text field of testing that allows a user to input text.
+radio buttons to allow the user to dynamically change the chosen text's alignment (Left, Center, and Right.)
+and an Additional text field will allow the user to change the initial testing text field's column size through the input of integers.
+
+### Testing
+
+After the inital layout was complete a test run was used to make sure everything was in the correct place.
+The radio buttons were then added to a group and tested for interaction.
+another test was done after the radio buttons and column size text field were both given functionality.
+The final test was done after adjustiing the scene size a bit and everything was working.
+
+## Adapted from a README Built With
+
+* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone who's code was used
+* Inspiration
+* etc
---
+++ Demonstrate TextField properties/src/demonstrate/textfield/properties/DemonstrateTextFieldProperties.java
@@ -0,0 +1,87 @@
+package demonstrate.textfield.properties;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Ryley Green
+ */
+public class DemonstrateTextFieldProperties extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        //Create two text fields
+        TextField tfTextField = new TextField();
+        TextField tfPrefColumnSize = new TextField();
+        tfTextField.setAlignment(Pos.BOTTOM_RIGHT);
+        tfPrefColumnSize.setAlignment(Pos.BOTTOM_RIGHT);
+        tfTextField.setPrefColumnCount(15);
+        tfPrefColumnSize.setPrefColumnCount(5);
+        
+        //Place testing text field into top center
+        HBox hBox1 = new HBox(5);
+        hBox1.setAlignment(Pos.CENTER);
+        hBox1.getChildren().addAll(new Label("Text Field"), tfTextField);
+        
+        //Create Radio Buttons
+        RadioButton rbLeft = new RadioButton("Left");
+        RadioButton rbCenter = new RadioButton("Center");
+        RadioButton rbRight = new RadioButton("Right");
+        
+        //Create group for radio buttons
+        ToggleGroup group = new ToggleGroup();
+        rbLeft.setToggleGroup(group);
+        rbCenter.setToggleGroup(group);
+        rbRight.setToggleGroup(group);
+        
+        //Place radio buttons bottom center
+        HBox hBox2 = new HBox(5);
+        hBox2.setAlignment(Pos.CENTER);
+        hBox2.getChildren().addAll(rbLeft, rbCenter, rbRight);
+        
+        //Label and add text field to change testing box size
+        HBox hBox3 = new HBox(5);
+        hBox3.getChildren().addAll(new Label("Column Size"), tfPrefColumnSize);
+        
+        //Combine layouts
+        HBox hBox4 = new HBox(15);
+        hBox4.setAlignment(Pos.CENTER);
+        hBox4.getChildren().addAll(hBox2, hBox3);
+        VBox vBox = new VBox(5);
+        vBox.getChildren().addAll(hBox1, hBox4);
+        
+        //Set scene
+        Scene scene = new Scene(vBox, 350, 60);
+        primaryStage.setTitle("Radio Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+        //Add functionality to radio buttons and second text field using event handlers
+        tfPrefColumnSize.setOnAction(e -> {
+            tfTextField.setPrefColumnCount(
+                    Integer.parseInt(tfPrefColumnSize.getText())
+            );
+        });
+        rbLeft.setOnAction(e -> tfTextField.setAlignment(Pos.BASELINE_LEFT));
+        rbCenter.setOnAction(e -> tfTextField.setAlignment(Pos.BASELINE_CENTER));
+        rbRight.setOnAction(e -> tfTextField.setAlignment(Pos.BASELINE_RIGHT));
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
